74502af94fb8d2c6d064df82a44c19d3f31acaea
Jonathan Schell <jschell.21.09.87@gmail.com>
2019-10-04 16:15:41 -0300

    rest api implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const {hub} = require('./urls');
+const {hub, requiredIsEmpty} = require('./urls');
 
 // activo cors para habilitar el paso de cualquier dominio
 app.use(cors());
@@ -10,8 +10,23 @@ app.use(cors());
 app.use(express.static('public'));
 
 // creo middelware que se encargará de controlar los tokens
-app.use('/', (req, res, next) => {
-    next();
+app.use('/api/*', (req, res, next) => {
+    // console.log(req.headers.authorization);
+    
+    // checkeo qué urls no necesitan comprobar el token
+    switch (req.baseUrl) {
+        case '/api/app_authorization':
+            next();
+        break;
+    
+        default:
+            if(req.headers.authorization){
+                next();
+            }else{
+                res.status(400).json(requiredIsEmpty('Falta token!'));
+            }
+        break;
+    }
 })
 
 // paso todas las peticiones por el hub de urls
--- urls/index.js
+++ urls/index.js
@@ -1,19 +1,132 @@
 const path = require('path');
+const {
+    appAuthorization, 
+    authenticationUser, 
+    getUserInfo, 
+    searchRestaurants 
+} = require('../api/endpoints');
 
 const hub = (app) => {
     
-    // console.log(app);
+    // Home, simplemente enlaces
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'))
     });
 
+    // rest help, una guía para el uso del api rest
     app.get('/rest_help', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/rest_help.html'))
     });
+
+    // App Authorization
+    app.get('/api/app_authorization', (req, res) => {
+
+        const args = {
+            clientId: req.query.clientId,
+            clientSecret: req.query.clientSecret
+        }
         
+        // Funcion que checkea los parámetros y en caso de que estén OK envía petición al Api
+        sendToApi(res, args, ['clientId', 'clientSecret'], appAuthorization(args));
+
+    });
+
+    // User Authentication
+    app.get('/api/authentication_user', (req, res) => {
+        
+        const args = {
+            userName: req.query.userName,
+            password: req.query.password
+        }
+
+        const token = req.headers.authorization;
+
+        // Funcion que checkea los parámetros y en caso de que estén OK envía petición al Api
+        sendToApi(res, args, ['userName', 'password'], authenticationUser(args, token));
+
+    });  
+    
+    // Get User Info
+    app.get('/api/get_user_info', (req, res) => {
+        
+        const token = req.headers.authorization;
+
+        // Funcion que checkea los parámetros y en caso de que estén OK envía petición al Api
+        sendToApi(res, {}, [], getUserInfo(token));
+
+    });
+
+    // Búsqueda de restaurantes según cords
+    app.get('/api/search_restaurants', (req, res) => {
+        
+        const args = {
+            point : req.query.point,
+            country : req.query.country, 
+            max : req.query.max, 
+            offset: req.query.offset, 
+            sort : req.query.sort, 
+            count : req.query.count, 
+            fields : req.query.fields
+        }
+
+        const token = req.headers.authorization;
+
+        // Funcion que checkea los parámetros y en caso de que estén OK envía petición al Api
+        sendToApi(res, args, ['point', 'country'], searchRestaurants(args, token));
+
+    });  
     
 }
 
+const checkRequiredParams = (args, requires) => {
+
+    for(const i in args){
+        if(requires.includes(i) && i != undefined){
+            if(!args[i]){
+                return false;
+            }
+        }
+    }
+
+    return true;
+
+}
+
+const requiredIsEmpty = (message) => {
+    return {
+        error : true,
+        message : message
+    }
+}
+
+const sendToApi = (res, args, requires, callback) => {
+
+    // checkeo que los parámetros obligatorios están siendo enviádos
+    if(checkRequiredParams(args, requires)){
+
+        // Envío datos al api
+        callback.then(data => {
+            
+            // ok, 200
+            res.status(200).json(data);
+
+        }).catch(err => {
+
+            // error, 400
+            res.status(400).json(err);
+
+        })
+
+    }else{
+        
+        // error, 400
+        res.status(400).json(requiredIsEmpty('Faltan parámetros requeridos.'));
+
+    }
+
+}
+
 module.exports = {
-    hub
+    hub,
+    requiredIsEmpty
 }
